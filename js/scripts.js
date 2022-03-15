c1cd5aae89acee9009ac68b062539ca6822fc292
Charl <94119475+Charl1980@users.noreply.github.com>
2022-03-15 14:12:51 +0100

    If applied, this commit have added JavaScript code relevant to the Pokedex Achievement.

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+let pokemonList = [
+  {
+    name: 'Bulbasaur',
+    height: 0.7,
+    types: ['Grass', 'Poison'];
+  }
+  {
+    name: 'Charmander',
+    height: 0.6,
+    types: 'Fire'
+  }
+  {
+    name: 'Squirtle',
+    height: 0.5,
+    types: 'Water'
+  }
+  {
+    names: 'Pikachu',
+    height: 0.4,
+    types: 'Electric'
+  }
+];
