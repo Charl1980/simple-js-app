2271c7c473fccb62c27768bee76e300eb6debdba
Charl <94119475+Charl1980@users.noreply.github.com>
2022-03-24 14:20:54 +0100

    If applied, this commit will wrap the pokemonList in an IIFE, create a pokemonRepository, add public functions and update the forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,59 @@
-//Pokemon List Array with nested Objects
-let pokemonList = [
-  {
-    name: 'Bulbasaur',
-    height: 0.7,
-    types: ['Grass', 'Poison']
-  },
-  {
-    name: 'Charmander',
-    height: 0.6,
-    types: 'Fire'
-  },
-  {
-    name: 'Squirtle',
-    height: 0.5,
-    types: 'Water'
-  },
-  {
-    name: 'Pikachu',
-    height: 0.4,
-    types: 'Electric'
-  },
-];
+// IIFE
+let pokemonRepository = (function() {
 
-// forEach() Loop code refactoring
-pokemonList.forEach(function(pokemon) {
+  //Pokemon List Array with nested Objects
+  let pokemonList = [{
+      name: 'Bulbasaur',
+      height: 0.7,
+      types: ['Grass', 'Poison']
+    },
+    {
+      name: 'Charmander',
+      height: 0.6,
+      types: 'Fire'
+    },
+    {
+      name: 'Squirtle',
+      height: 0.5,
+      types: 'Water'
+    },
+    {
+      name: 'Pikachu',
+      height: 0.4,
+      types: 'Electric'
+    },
+  ];
+
+  // Public functions
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+// forEach() Loop code refactoring with .getAll & .add key/values
+pokemonRepository.getAll().forEach(function(pokemon) {
   if (pokemon.height < 0.7) {
-  document.write('<p>' + pokemon.name + ' ' + '(' + pokemon.height + ')</p>');
+    document.write('<p>' + pokemon.name + ' ' + '(' + pokemon.height + ')</p>');
+  } else if (pokemon.height >= 0.7) {
+    document.write('<p>' + pokemon.name + ' ' + '(' + pokemon.height + ')' + ' - Wow, that\'s big!</p>');
+  }
+});
+pokemonRepository.add({
+  name: 'Zubat',
+  height: 0.8,
+  types: ['Poison', 'Flying']
+});
+pokemonRepository.getAll().forEach(function(pokemon) {
+  if (pokemon.height < 0.7) {
+    document.write('<p>' + pokemon.name + ' ' + '(' + pokemon.height + ')</p>');
   } else if (pokemon.height >= 0.7) {
     document.write('<p>' + pokemon.name + ' ' + '(' + pokemon.height + ')' + ' - Wow, that\'s big!</p>');
   }
